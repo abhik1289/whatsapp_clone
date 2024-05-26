918ecc003b397eb9513548321670b4b7a5bd1d70
Abhik <theabhik2020@gmail.com>
2024-05-26 17:04:27 +0530

    set_up_package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import mongoSanitize from "express-mongo-sanitize";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 dotenv.config();
-import  "./utils/db";
-
+import "./utils/db";
 let PORT = process.env.PORT;
+// create express app
 const app = express();
+//morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+// parser json request url
 app.use(express.json());
+// parser json response body
 app.use(express.urlencoded({ extended: false }));
-
+// sanitizer request data
+app.use(mongoSanitize());
+// file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+//cors
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+  })
+);
+//define server PORT
 app.listen(5000, () => {
   console.log("Server listening on port", PORT);
 });
--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
+const database_url: string | undefined = process.env.DB_URL || "mongodb://localhost:27017/whatsapp";
 
-
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect(database_url)
     .then(() => console.log('Connected!')).catch((err) => console.log(err));
